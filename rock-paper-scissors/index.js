766d4c48b63f7cfd80835ab534815af60e24bd00
TarekVisch <tarek.chentouf.99@gmail.com>
2022-03-22 18:40:05 +0100

    feat: add round logic with win and lose conditions

--- rock-paper-scissors/index.js
+++ rock-paper-scissors/index.js
@@ -20,3 +20,40 @@ function computerPlay() {
 
   return computerSelection;
 }
+
+function playRound(playerSelection, computerSelection) {
+  let result = { message: "", roundWinner: "" };
+
+  if (playerSelection === computerSelection) {
+    result.message = "It's a tie";
+  } else if (playerSelection === ROCK) {
+    if (computerSelection === SCISSORS) {
+      result.message = "You Win! Rock beats Scissors";
+      result.roundWinner = "Player";
+    } else if (computerSelection === PAPER) {
+      result.message = "You Loose! Paper beats Rock";
+      result.roundWinner = "Computer";
+    }
+  } else if (playerSelection === PAPER) {
+    if (computerSelection === ROCK) {
+      result.message = "You Win! Paper beats Rock";
+      result.roundWinner = "Player";
+    } else if (computerSelection === SCISSORS) {
+      result.message = "You Loose! Scissors beats Paper";
+      result.roundWinner = "Computer";
+    }
+  } else if (playerSelection === SCISSORS) {
+    if (computerSelection === PAPER) {
+      result.message = "You Win! Scissors beats Paper";
+      result.roundWinner = "Player";
+    } else if (computerSelection === ROCK) {
+      result.message = "You Loose! Rock beats Scissors";
+      result.roundWinner = "Computer";
+    }
+  } else {
+    result.message = "Invalid input";
+    result.roundWinner = "Computer";
+  }
+
+  return result;
+}
