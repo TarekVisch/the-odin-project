6e2b53fd45bafea251cb561bdff9ce6d7a7a7994
TarekVisch <tarek.chentouf.99@gmail.com>
2022-03-22 18:36:47 +0100

    feat: return random computer guess

--- rock-paper-scissors/index.js
+++ rock-paper-scissors/index.js
@@ -1,1 +1,22 @@
-console.log("Hello world");
+const ROCK = "rock";
+const PAPER = "paper";
+const SCISSORS = "scissors";
+
+function computerPlay() {
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+  let computerSelection = "";
+
+  switch (randomNumber) {
+    case 1:
+      computerSelection = ROCK;
+      break;
+    case 2:
+      computerSelection = PAPER;
+      break;
+    case 3:
+      computerSelection = SCISSORS;
+      break;
+  }
+
+  return computerSelection;
+}
